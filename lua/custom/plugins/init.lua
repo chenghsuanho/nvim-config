eecd4eb3ebf8aed6ebf354fa4a29d6b8bea32957
Chenghsuan Ho <chengxuanjhe@gmail.com>
2023-10-06 16:10:01 +0800

    First customization

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,25 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Switch Input Method automatically depends on Neovim's edit mode.
+  {
+    "keaising/im-select.nvim",
+    Lazy = false,
+    config = function()
+      require("im_select").setup({})
+    end,
+  },
+
+  -- Surround text with pairs of symbols.
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  }
+}
+
+-- vim: ts=2 sts=2 sw=2 et
