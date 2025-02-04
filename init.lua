d1c75124a17aefa019e0d31092b0977c6af93d5f
Riley Ho <riley@rileychh.dev>
2025-02-04 13:47:53 +0800

    Don't automatically insert the current comment leader

--- init.lua
+++ init.lua
@@ -118,6 +118,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Don't automatically insert the current comment leader
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = "Don't automatically insert the current comment leader",
+  group = vim.api.nvim_create_augroup('no-autoinsert-comment-leader', { clear = true }),
+  callback = function()
+    vim.opt.formatoptions:remove { 'c', 'r', 'o' }
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
